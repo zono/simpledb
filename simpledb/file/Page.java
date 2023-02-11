281d7ebd2753fe547303e8490973cb6ef86b4fb1
Yusaku Maezono <yusakumaezono@gmail.com>
2023-02-11 20:08:10 +0900

    Add File Manager.

---
+++ simpledb/file/Page.java
@@ -0,0 +1,62 @@
+package simple.file;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.*;
+
+public class Page {
+  private ByteBuffer bb;
+  public static Charset CHARSET = StandardCharsets.US_ASCII;
+
+  // For creating data buffers
+  public Page(int blocksize) {
+    bb = ByteBuffer.allocateDirect(blocksize);
+  }
+
+  // For creating log pages
+  public Page(byte[] b) {
+    bb = ByteBuffer.wrap(b);
+  }
+
+  public int getInt(int offset) {
+    return bb.getInt(offset);
+  }
+
+  public void setInt(int offset, int n) {
+    bb.putInt(offset, n);
+  }
+
+  public byte[] getBytes(int offset) {
+    bb.postition(offset);
+    int  length = bb.getInt();
+    byte[] b = new byte[length];
+    bb.get(b);
+    return b;
+  }
+
+  public void setBytes(int offset, byte[] b) {
+    bb.postition(offset);
+    bb.putInt(b.length);
+    bb.put(b);
+  }
+
+  public String getString(int offset) {
+    byte[] b = getBytes(offset);
+    return new String(b, CHARSET);
+  }
+
+  public void setString(int offset, String s) {
+    byte[] b = s.getBytes(CHARSET);
+    setBytes(offset, b);
+  }
+
+  public static int maxLength(int strlen) {
+    float bytesPerChar = CHARSET.newEncoder().maxBytesPerChar();
+    return Integer.BYTES + (strlen * (int)bytesPerChar);
+  }
+
+  // a package private method, needed by FileMgr
+  ByteBuffer contents() {
+    bb.postition(0);
+    return bb;
+  }
+}
